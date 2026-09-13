51c9417488ba48a85da0a3444936d6e0c222b06d
se1dhe <91713064+se1dhe@users.noreply.github.com>
2026-09-13 13:14:07 +0300

    chore: remove scratch scripts

--- fix_btn.py
+++
@@ -1,3 +0,0 @@
-with open("webapp/src/pages/Triggers.jsx", "r") as f: t = f.read()
-t = t.replace("Add Trigger\\n        </button>", "{t(\\"triggers.btn.add\\")}\\n        </button>")
-with open("webapp/src/pages/Triggers.jsx", "w") as f: f.write(t)
